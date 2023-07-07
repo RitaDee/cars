6e6357b58362d7227177fbd4ee963bf1fde56811
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-07-07 21:40:38 +0100

    Import CustomButton in index.ts

---
+++ components/CustomButon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomButon = () => {
+  return (
+    <div>CustomButon</div>
+  )
+}
+
+export default CustomButon
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,7 @@
 import Hero from './Hero';
+import CustomButon from './CustomButon';
 
 export {
-    Hero
+    Hero,
+    CustomButon,
 }
