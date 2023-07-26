1c57cee779dee3d864f9a4db14c26b9cfe8f3a6a
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-07-26 14:19:10 +0100

    Add overlay to hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,6 +27,8 @@ const Hero = () => {
         <div className="hero__image-container">
           <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
+
+          <div className="hero__image-overlay" />
           </div>
         </div>
     </div>
