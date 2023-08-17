d18ac50e18f772857c0cb285ad345e38e508ef83
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-17 21:42:01 +0100

    Update Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image src="\logo.svg" alt="Car hub logo"
+          width={118}
+          height={18}
+          className="object-contain"
+           />
+
+        </div>
+
+      </div>
+
+    </footer>
   )
 }
 
