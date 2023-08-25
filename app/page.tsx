996129f85486ffac187a59db65860c77d59ec849
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-25 18:55:55 +0100

    Update pages.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Hero } from '@/components'
-import Image from 'next/image'
+import { CustomFilter, Hero, SearchBar } from '@/components';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Auto Inventory</h1>
-          <p>Discover the cars that could interest you.</p>
+          <p>Discover the cars that could interest you</p>
         </div>
         <div className="home__filters">
           <SearchBar />
