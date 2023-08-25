fdca021c35ca7980dfab690f1f8d5b260c76e796
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-25 18:43:06 +0100

    Set up UI layout for car discovery section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,21 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Auto Inventory</h1>
+          <p>Discover the cars that could interest you.</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
