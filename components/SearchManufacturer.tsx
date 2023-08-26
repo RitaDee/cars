58b82d08db5e03563b263420dc3f3773661273b9
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-26 20:52:35 +0100

    style: Add car logo to Combobox button

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,25 @@
+"use client";
+import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 import { SearchManufacturerProps } from '@/types';
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacturer">
+        <Combobox>
+            <div className="relative w-full">
+                <Combobox.Button className="absolute top-[14px]">
+                    <Image
+                    src="\car-logo.svg"
+                    width={20}
+                    height={20}
+                    className="ml-4"
+                    alt="car-logo"
+                     />
+                </Combobox.Button>
+            </div>
+        </Combobox>
+    </div>
   )
 }
 
