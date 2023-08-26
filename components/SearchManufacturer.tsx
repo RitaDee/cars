196f0a0c1c990f17d2e92c5ceaeeca75d05bf438
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-26 20:38:21 +0100

    refactor: Destructure props in SearchManufacturer component

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import { Combobox, Transition } from "@headlessui/react";
+import { SearchManufacturerProps } from '@/types';
 
-const SearchManufacturer = ({manufacturer, setManufacturer} : ) => {
+const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
   return (
     <div>SearchManufacturer</div>
   )
