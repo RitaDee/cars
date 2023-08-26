7bd20821f21e4c0aa86dd176e37329ce0a87ccab
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-26 21:07:31 +0100

    feat: Implement input element with state management

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 import { SearchManufacturerProps } from '@/types';
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+    const [query, setQuery] = useState("");
   return (
     <div className="search-manufacturer">
         <Combobox>
@@ -21,6 +23,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
                 className="search-manufacturer__input"
                 placeholder="Volkswagen"
                 displayValue={(manufacturer: string) => manufacturer}
+                onChange={(e) => setQuery(e.target.value)}
                 />
             </div>
         </Combobox>
