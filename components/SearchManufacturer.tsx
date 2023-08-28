fa1459e11a390b8ec9132c2a204708c505805de0
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-28 23:53:57 +0100

    style: improve styling for selected options

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -15,7 +15,8 @@ const SearchManufacturer = ({
     query === ""
       ? manufacturers
       : manufacturers.filter((item) =>
-          item.toLowerCase()
+          item
+            .toLowerCase()
             .replace(/\s+/g, "")
             .includes(query.toLowerCase().replace(/\s+/g, ""))
         );
@@ -47,19 +48,37 @@ const SearchManufacturer = ({
             afterLeave={() => setQuery("")}
           >
             <Combobox.Option>
-              { filteredManufacturers.map((item) => (
-                  <Combobox.Option
-                    key={item}
-                    className={({ active }) => `
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) => `
                     relative search-manufacturer__option
                     &{active} 'bg-primary-blue text-white' : 'text-gray-900'
                     `}
-                    value={item}
-                  >
-                    {item}
-                  </Combobox.Option>
-                )
-                )}
+                  value={item}
+                >
+                  {({ selected, active }) => {
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+
+                      {/* Show an active blue background color if the option is selected */}
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-pribg-primary-purple"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>;
+                  }}
+                </Combobox.Option>
+              ))}
             </Combobox.Option>
           </Transition>
         </div>
