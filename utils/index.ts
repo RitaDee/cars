452c9302d1c44061025a82595004290f7feb6277
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-31 00:54:44 +0100

    chore(api): update car data fetch configuration

---
+++ utils/index.ts
@@ -0,0 +1,14 @@
+export async function fetchCars() {
+  const headers = {
+		'X-RapidAPI-Key': '0529cc2e2dmshbc096509c9e9b4cp19b15fjsncc841b818bb3',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+  const response  = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+    headers: headers,
+});
+
+  const result = await response.json();
+
+  return result;
+}
+
