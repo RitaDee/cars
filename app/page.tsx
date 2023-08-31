556a579fe84b4d6238cd12699ad1d124e448d5cb
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-31 01:01:25 +0100

    feat: integrate car data fetching and logging

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { CustomFilter, Hero, SearchBar } from '@/components';
+import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  console.log(allCars);
   return (
     <main className="overflow-hidden">
       <Hero />
