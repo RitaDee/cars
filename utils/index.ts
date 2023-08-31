fff30ccb0f42b610102dba8b6f9509562f5fde69
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-31 15:42:23 +0100

    feat: hide api key

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 export async function fetchCars() {
   const headers = {
-		'X-RapidAPI-Key': '0529cc2e2dmshbc096509c9e9b4cp19b15fjsncc841b818bb3',
+		'X-RapidAPI-Key': process.env.SECRET_KEY as string,
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
   const response  = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
