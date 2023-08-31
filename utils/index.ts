20b3e93d6ca5a62500758460784592938e375859
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-31 01:05:29 +0100

    feat: add model parameter to car data fetch URL

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Key': '0529cc2e2dmshbc096509c9e9b4cp19b15fjsncc841b818bb3',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-  const response  = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+  const response  = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
     headers: headers,
 });
 
