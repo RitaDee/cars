18d0407157793520eea910bd7032f85b5a53a04c
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-14 20:44:51 +0100

    refactor: Update modal close button styling and animation

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,23 +1,22 @@
-"use client"
-import React from 'react';
-import Image from 'next/image';
-import { Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { CarProps } from '@/types';
-
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CarProps } from "@/types";
 
 interface CarDetailsProps {
-    isOpen: boolean;
-    closeModal: () => void;
-    car: CarProps;
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
 }
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
     <>
-    <Transition appear show={isOpen} as={Fragment}>
+      <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-            <Transition.Child
+          <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0"
@@ -25,13 +24,42 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
             leave="ease-in duration-200"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            >
-                <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-        </Dialog>
-    </Transition>
-    </>
-  )
-}
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
 
-export default CarDetails
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel>
+                  <button
+                  type="button"
+                  onClick={closeModal}
+                  >
+                    <Image 
+                    src="/close.svg"
+                    alt="close"
+                    width={20}
+                    height={20}
+                    className="object-contain"
+                    />
+                  </button>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default CarDetails;
