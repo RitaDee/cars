6356ea408526a10a1ab7b28c78e793e7bb41f11a
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-15 13:01:01 +0100

    style: Update image containers for car models on the homepage

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -39,9 +39,10 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel>
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[95vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                   type="button"
+                  className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
                   onClick={closeModal}
                   >
                     <Image 
@@ -52,6 +53,26 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     className="object-contain"
                     />
                   </button>
+
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+                    </div>
+
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+                      </div>
+
+                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+                      </div>
+
+                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+                      </div>
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
