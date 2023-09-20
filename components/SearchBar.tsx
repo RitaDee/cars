ad7e3b56157d3b2dfa307c42c548785af93b234d
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-20 14:35:10 +0100

    style: hide search button on specific screen sizes

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -21,34 +21,33 @@ const SearchBar = () => {
 
     const handleSearch = () => {}
   return (
-    <form className="searchbar" onSubmit={handleSearch}>
-        <div className="searchbar__items">
-            <SearchManufacturer 
-            manufacturer = {manufacturer}
-            setManufacturer = {setManufacturer}
-            />
-
-            <SearchButton otherClasses="sm:hidden" />
-        </div>
-
-        <div className="searchbar__item">
-          <Image
-          src="/model-icon.png"
+     <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManufacturer}
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
           width={25}
           height={25}
-          className="absolute w-5 h-5 ml-4"
-          alt="car model"
-           />
-
-           <input 
-           type="text"
-           name="model"
-           value={model}
-           onChange={(e) => setModel(e.target.value)}
-           placeholder="Tiguan..."
-           className="searchbar__input"
-            />
-        </div>
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
   )
 }
