2d09d92c496ab4ca93c6c05d8240f8f8447a10db
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-20 14:18:52 +0100

    feat: add search button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,19 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 import { SearchManufacturer } from "./";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
 
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState("");
@@ -12,6 +25,8 @@ const SearchBar = () => {
             manufacturer = {manufacturer}
             setManufacturer = {setManufacturer}
             />
+
+            <SearchButton otherClasses="sm:hidden" />
         </div>
     </form>
   )
--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-  const response  = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera`, {
+  const response  = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3`, {
     headers: headers,
 });
 
@@ -29,20 +29,6 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
-// export const generateCarImageUrl = (car: CarProps, angle? :string) => {
-//   const url = new URL('https://cdn.imagin/studio/getimage');
-
-//   const { make, year, model } = car;
-
-//   url.searchParams.append('customer', 'hrjavascript-mastery')
-//   url.searchParams.append('make', make);
-//   url.searchParams.append('modelFamily', model.split(' ')[0]);
-//   url.searchParams.append('zoomType', 'fullscreen');
-//   url.searchParams.append('modelYear', `${year}`);
-//   url.searchParams.append('angle', `${angle}`);
-
-//   return `${url}`
-// }
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
