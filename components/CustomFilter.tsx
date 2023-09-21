f080ca3633aabd96329dd3646f472de91bcd43ba
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-21 04:10:03 +0100

    feat: add Listbox for filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,10 +3,19 @@ import { Fragment, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Listbox, Transition } from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
 
-const CustomFilter = ({ title, option}) => {
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
   return (
-    <div>CustomFilter</div>
+    <div className="w-fit">
+      <Listbox>
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span>Filter</span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
