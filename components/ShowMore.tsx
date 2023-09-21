1769d03e2864014ff80c111404829d98fa822188
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-21 13:23:02 +0100

    feat: implement handleNavigation function

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,8 +1,20 @@
 "use clientSide";
 
+import { ShowMoreProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 import { useRouter } from "next/navigation";
 
-const ShowMore = ({ pageNumber, isNext}) => {
+const ShowMore = ({ pageNumber, isNext}: ShowMoreProps) => {
+    const router = useRouter();
+
+    const handleNavigation = () => {
+      const newLimit = (pageNumber + 1) * 10;
+
+      const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+      router.push(newPathname);
+    };
+
   return (
     <div>ShowMore</div>
   )
