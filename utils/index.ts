860ae23e1631bf6ded6edad241b25e0bb3e1eaf0
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-21 03:39:59 +0100

    feat: update API request to fetch cars based on filters

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,17 @@
-import { CarProps } from '@/types';
+import { CarProps, FilterProps } from '@/types';
 
 export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
   const headers = {
 		'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-  const response  = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3`, {
-    headers: headers,
-});
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
 
   const result = await response.json();
   
