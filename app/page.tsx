5d297bfbe859a0e09590c3f4d24620c21375431e
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-09-21 13:12:33 +0100

    feat: add ShowMoreProps interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components';
 import { fuels, yearsOfProduction } from '@/components/constants';
 import { fetchCars } from '@/utils';
 import Image from 'next/image';
@@ -38,6 +38,11 @@ export default async function Home({ searchParams }) {
               {allCars?.map((car) => 
               <CarCard car={car} />)}
             </div>
+
+            <ShowMore
+            pageNumber={(searchParams.pageNumber || 10) / 10}
+            isNext={(searchParams.limit || 10) > allCars.length}
+             />
           </section>
         ) : (
           <div className="home__error-container">
---
+++ components/ShowMore.tsx
@@ -0,0 +1,11 @@
+"use clientSide";
+
+import { useRouter } from "next/navigation";
+
+const ShowMore = ({ pageNumber, isNext}) => {
+  return (
+    <div>ShowMore</div>
+  )
+}
+
+export default ShowMore
